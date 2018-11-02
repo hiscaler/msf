aab9c55e5dbc05714624a525c4b10e08f4e1e02b
hiscaler <hiscaler@gmail.com>
2018-11-02 23:02:12 +0800

    ...

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {Passport} from '../../models/Passport';
-import {PassportService} from '../../services/passport.service';
-import {FlashMessageService} from '../../services/flash-message.service';
+import { Component, OnInit } from '@angular/core';
+import { Passport } from '../../models/Passport';
+import { PassportService } from '../../services/passport.service';
+import { FlashMessageService } from '../../services/flash-message.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   passport: Passport = {
